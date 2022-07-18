00b13453a62356925ec3c74eea6f7e405590dd4a
Jason <jasoncmcvicar@gmail.com>
2022-07-18 18:10:51 -0500

    working through kinks with async getting them doggies

--- src/App.js
+++ src/App.js
@@ -7,18 +7,28 @@ import { useState } from "react";
 import DogDetails from "./DogDetails";
 import DogList from "./DogList";
 
-const DOGS_URL = "http://localhost:5001/dogs"
+const DOGS_URL = "http://localhost:5001/dogs";
+
 
 // FIXME: make sure names is passed as a prop to Nav
 function App() {
+  const [dogs, setDogs] = useState([]);
+  //TODO: use Loading state
+
+  console.log("in the App component ", dogs);
+
   async function getDogs() {
     let response = await axios.get(DOGS_URL);
-    console.log("getDogs response", response);
-    return response.data;
+    //console.log("getDogs response", response);
+    setDogs(response.data);
   }
-  
-  const [dogs, setDogs] = useState(getDogs);
-  console.log("App", "State:", dogs);
+
+  //console.log("App", "State:", dogs);
+  if (dogs.length === 0){
+    getDogs();
+    return (<h1>Loading</h1>);
+  }
+
 
   return (
     <div className="App">
